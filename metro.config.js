169c8f0b097d3372e84bff0691eae111e942994c
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-04-12 14:35:34 -0300

    create project and config native wind

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,8 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro')
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = mergeConfig(getDefaultConfig(__dirname), {
+  /* your config */
+});
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withNativeWind(config, { input: './src/styles/global.css'})
