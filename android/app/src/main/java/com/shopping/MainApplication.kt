c72784a7eca0db7bf929431456c414eccac432f7
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-04-26 16:53:14 -0300

    feat: big change

--- android/app/src/main/java/com/shopping/MainApplication.kt
+++ android/app/src/main/java/com/shopping/MainApplication.kt
@@ -11,7 +11,6 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
-import com.oblador.vectoricons.VectorIconsPackage;
 
 class MainApplication : Application(), ReactApplication {
 
@@ -20,7 +19,6 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
